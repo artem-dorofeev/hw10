b44903ac7b8a45963231d9e093b4c926ad1fca89
Artem Dorofeev <a.dorofeev@ukr.net>
2023-07-16 19:02:51 +0300

    hw10

--- main.py
+++ main.py
@@ -1,89 +1,87 @@
-from sanitize import sanitize_phone_number
 from classes_bot import AddressBook, Name, Phone, Record
+from sanitize import sanitize_phone_number
 
-
-contacts_dict = AddressBook()
+address_book = AddressBook()
 
 
 def input_error(func):
-    def wrapper(x):
+    def wrapper(*args):
         try:
-            result = func(x)
+            result = func(*args)
         except IndexError:
             return f"Введіть ім'я та номер контакту"
         return result
     return wrapper
 
 
+@input_error
+def add_command(*args):
+    name = Name(args[0].capitalize())
+    phone = Phone(sanitize_phone_number(args[1]))
+    rec: Record = address_book.get(str(name))
+    if rec:
+        return rec.add_phone(phone)
+    rec = Record(name, phone)
+    return address_book.add_record(rec)
+
+
 def index_error(func):
-    def wrapper(x):
+    def wrapper(*args):
         try:
-            result = func(x)
+            result = func(*args)
         except IndexError:
-            return f'Немає імені контакту'
+            return f"Немає імені або номеру контакта"
         return result
     return wrapper
 
 
-def hello_func(text=None):
-    return f'Привіт, чим можу допомогти?'
+@index_error
+def phone_print(*data):
+    contact = data[0].capitalize().strip()
+    for key, val in address_book.items():
+        
+        if contact == key:
+            print(key)
+            return f"Контакт: {val}"
+    return f"Немає {contact} в списку контактів"
 
-
-@input_error
-def add_func(args: list):
-    name = Name(args[0].capitalize())
-    phone = Phone(sanitize_phone_number(args[1]))
-    rec: Record = contacts_dict.get(str(name))
-    if rec:
-        return rec.add_phone(phone)
-    rec = Record(name, phone)
-    contacts_dict.add_record(rec)
-    return f'До адресної книги додан контакт {name.value} - {phone.value}'
-
-
-def show_all(text=None):
-    return contacts_dict
-
-
-@input_error
-def change_command(args: list):
+@index_error
+def change_command(*args):
     name = Name(args[0].capitalize())
     old_phone = Phone(sanitize_phone_number(args[1]))
     new_phone = Phone(sanitize_phone_number(args[2]))
-    rec: Record = contacts_dict.get(str(name))
+    rec: Record = address_book.get(str(name))
     if rec:
         return rec.change_phone(old_phone, new_phone)
     return f"Немає {name} в списку кнтактів"
 
-
-@index_error
-def phone_print(data):
-    contact = data[0].capitalize().strip()
-    for key, val in contacts_dict.items():
-        
-        if contact == key:
-            print(key)
-            return f'Контакт: {val}'
-    return f'Немає {contact} в списку контактів'
-
+def hello_func(text=None):
+    return f"Привіт, чим можу допомогти?"
 
 def exit_command(*args):
-    return f'До побачення'
+    return "До побачення"
+
 
 def unknown_command(*args):
-    return f'Невідома команда'
+    return f"Невідома команда"
 
-COMMAND_DICT = {
+
+def show_all_command(*args):
+    return address_book
+
+
+COMMANDS = {
+    add_command: ("add", "+", "додати"),
+    change_command: ("change", "зміни"),
+    exit_command: ("до побачення", "до зустрічі", "exit", "by", "пока", "end"),
+    show_all_command: ("show all", "показати все"),
     hello_func: ("hello", "hi", "привіт"),
-    add_func: ("add", "+", "додати"),
-    show_all: ("show all", "показати все"),
-    change_command: ("change", "змінити"),
-    phone_print: ("phone", "друк"),
-    exit_command: ("до побачення", "до зустрічі", "exit", "by", "пока")
+    phone_print: ("phone", "друк", "print")
 }
 
-def parser_text(text:str):
-    for cmd, kwds in COMMAND_DICT.items():
+
+def parser(text:str):
+    for cmd, kwds in COMMANDS.items():
         for kwd in kwds:
             if text.lower().startswith(kwd):
                 data = text[len(kwd):].strip().split()
@@ -91,16 +89,13 @@ def parser_text(text:str):
     return unknown_command, []
 
 def main():
-
     while True:
-
-        user_input = input('>>чекаю ввод>>')
-        cmd, data = parser_text(user_input)
-        result = cmd(data)
-        if cmd == exit_command:
-            print(result)
-            break
+        user_input = input(">>чекаю ввод>>")
+        cmd, data = parser(user_input)
+        result = cmd(*data)
         print(result)
+        if cmd == exit_command:
+            break
 
 if __name__ == "__main__":
     main()
